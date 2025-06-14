af671eb12ab4345cfe5cf5397cbf4faae8c54ee0
Cecell <mikesmail0000@gmail.com>
2025-06-14 07:48:59 -0500

    Update main.py
    Added code from Stormy

--- main.py
+++ main.py
@@ -32,3 +32,9 @@ def handle_request():
     ).execute()
 
     return jsonify({"file_id": created_file["id"]})
+
+import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
